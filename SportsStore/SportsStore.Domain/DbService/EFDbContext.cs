6063b31427692dcdc80d530aef6e417f7f7227ce
lukasz954 <lukasz.954@wp.pl>
2020-01-22 01:06:56 +0100

    Add EntityFramework to project

---
+++ SportsStore/SportsStore.Domain/DbService/EFDbContext.cs
@@ -0,0 +1,15 @@
+﻿using SportsStore.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Domain.DbService
+{
+    public class EFDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
